3f73e300bb9f407745a8cdffa1145123b3ca699d
Alfredo Lezama <lezamaa001@gmail.com>
2024-02-02 00:02:48 -0800

    Added exception handling to get author and barcode lookup functions and removed headless browser as well as selenium dependencies

--- barcode_v3.py
+++ barcode_v3.py
@@ -1,16 +1,10 @@
-import sys  
 import requests 
 import json
-import time 
 from bs4 import BeautifulSoup 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-#import chromedriver_binary
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 from database_handler import DataHandler
 from gui_class import ControlGui
-from time import sleep
+
 
 #This is the class that will be used to create the object
 # TO DO - change the title, auth, genre, etc to something more generic, so as to be able to be a BOOK or ITEM  
@@ -27,11 +21,12 @@ class BarcodeIntake(QObject):
         self.barcode_string = ""
         
         self.book_info = {
-                            "Title": '',
+                            'Title': '',
                             'Author': '',
                             'Genre' : '',
                             'ISBN' : '',
-                            'Publisher': ''
+                            'Publisher': '',
+                            'Publishing Date': ''
                         }
         self.titleEntry = []
         self.headers = {
@@ -60,9 +55,14 @@ class BarcodeIntake(QObject):
                  
         self._getAuthor()
         
-        for offer in data['items'][0]['offers']: # I want to get 5 different titles from the offer section And does not contain the word By or by 
-            if (len(self.titleEntry) < 5) & (offer["title"].title() not in self.titleEntry) & (f'{self.book_info['Author']}' not in offer["title"].title()):
-                self.titleEntry.append(offer["title"].title())
+        try:
+            for offer in data['items'][0]['offers']: # I want to get 5 different titles from the offer section And does not contain the word By or by 
+                if (len(self.titleEntry) < 5) & (offer["title"].title() not in self.titleEntry) & (f'{self.book_info['Author']}' not in offer["title"].title()):
+                    self.titleEntry.append(offer["title"].title())
+                    
+        except:
+            print('Error in barcode lookup')
+            return
                 
         self.book_info['Title'] = (max(self.titleEntry, key=len))
         self.book_info['ISBN'] = data['items'][0]['isbn']
@@ -78,6 +78,7 @@ class BarcodeIntake(QObject):
         display_list.append(self.book_info['Author'])
         display_list.append(self.book_info['Genre'])
         display_list.append(self.book_info['Publisher'])
+        display_list.append(self.book_info['Publishing Date'])
         
         print(f'inside barcode display list {display_list}')
         
@@ -90,42 +91,36 @@ class BarcodeIntake(QObject):
         
         url1 = "https://shop.harvard.com/book/"
         url2 = self.barcode_string
-        url = url1 + url2
-        
-        #driver = Service('usr/lib/chromium-browser/chromedriver') Raspberry pi
-        chrome_options = Options()
-        chrome_options.add_argument("--headless=new")
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(url)
-        print ("Headless Firefox Initialized")
+        url = url1 + url2        
 
-        # this is just to ensure that the page is loaded
-        time.sleep(1)  
-
-        # renders JS code and stores all info in static HTML code. 
-        html = driver.page_source 
+        # grabs the html from the page 
+        html = requests.get(url).text
 
         # Now, we could simply apply bs4 to html tag
         soup = BeautifulSoup(html, "html.parser") 
 
         #This gets Author, Genre, and Publisher, can also get publishing date (instead of edition)
-        relatedInfo = soup.find('fieldset', {'id' : 'aba-product-details-fieldset'})
-        publisher = relatedInfo.find(string='Publisher:').next_element.strip()
-        pubDate = relatedInfo.find(string='Publication Date:').next_element.strip()
-        
-        genreLocation = soup.find('fieldset',{'class': 'collapsible abaproduct-related-editions'})
-        genreInfo = genreLocation.findNext('a').text.strip()
-        
-        authorLocation = soup.find('div',{'class': 'author'}).text.strip()
-        author = authorLocation[3:]
-        
-        self.book_info['Author'] = author
-        self.book_info['Genre'] = genreInfo
-        self.book_info['Publisher'] = publisher
-        
+        try:
+            relatedInfo = soup.find('fieldset', {'id' : 'aba-product-details-fieldset'})
+            publisher = relatedInfo.find(string='Publisher:').next_element.strip()
+            pubDate = relatedInfo.find(string='Publication Date:').next_element.strip()
+            
+            genreLocation = soup.find('fieldset',{'class': 'collapsible abaproduct-related-editions'})
+            genreInfo = genreLocation.findNext('a').text.strip()
+            
+            authorLocation = soup.find('div',{'class': 'author'}).text.strip()
+            author = authorLocation[3:]
+            
+            self.book_info['Author'] = author
+            self.book_info['Genre'] = genreInfo
+            self.book_info['Publisher'] = publisher
+            self.book_info['Publishing Date'] = pubDate
+            
+        except:
+            print('Error in barcode lookup')            
+            return        
         
         print(f'Value of author inside getAuthor {author}')
-        driver.close()   
     
         
     @pyqtSlot(str, name="Scanned barcode")
@@ -140,8 +135,7 @@ class BarcodeIntake(QObject):
         else:
             return
     
-    def main_function(self):
-        
+    def main_function(self):        
         return
         
      
